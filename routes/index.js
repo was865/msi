8124e6f320db8326d88ef5874053d0d73ed4d567
was865 <865411640@qq.com>
2020-10-22 17:37:08 +0900

    add

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var sqlite3 = require('sqlite3');
 var moment = require('moment-timezone');
 var datautils = require('date-utils');
 const { response } = require('express');
+const { get } = require('./login');
 
 var knex = require('knex')({
   dialect:'sqlite3',
@@ -35,6 +36,21 @@ var shanaidata = Bookshelf.Model.extend({
 var contactdata = Bookshelf.Model.extend({
   tableName: 'contact',
 });
+
+var departmentdata = Bookshelf.Model.extend({
+  tableName: 'department_list',
+});
+
+var datadepartment;
+function getDepartment(){
+  new departmentdata().fetchAll().then((collection) => {
+    datadepartment = collection.toArray();
+  }).catch((err) => { 
+    response.status(500).json({error: true, data: {message: err.message}});
+  }); 
+  
+  return datadepartment;
+}
 
 var datastatus;
 function getStatus(){
@@ -86,6 +102,8 @@ function getMsg(){
 
 }
 
+//SQL router.GETはここから
+var db = new sqlite3.Database('ikisaki.sqlite3')
 router.get('/', function(req, res, next) {
 
   if (req.session.login == null) {
@@ -97,51 +115,175 @@ router.get('/', function(req, res, next) {
   getKyakusaki();
   getShanai();
   getMsg();
+  getDepartment();
 
-    var usertabledata = new Array();
+  var usertabledata = new Array();
+  var login = req.session.login;
 
-    new Userdata().orderBy('department')
-    .fetchAll()
-    .then((collection) => {
-        var content = collection.toArray();
+  var sql = 'SELECT * , CASE WHEN department = "' + login.department + '" THEN "AA" ELSE department END as sort1, CASE WHEN name = "' + login.name + '" THEN "00" ELSE name END as sort2 FROM users ORDER BY sort1,sort2 ASC;';
+  Bookshelf.knex.raw(sql).then((collection) => { 
 
-        content.forEach(element => {
+    collection.forEach(element => {
+      console.log("element : " + element);
+      var d1 = moment(new Date(element.updated_at));
+          d1.locale('ja');
+          d1.tz('Asia/Tokyo');
+      var dstr = d1.fromNow();
 
-            var d1 = moment(new Date(element.attributes.updated_at));
-                d1.locale('ja');
-                d1.tz('Asia/Tokyo');
-            var dstr = d1.fromNow();
+      usertabledata.push({
+        id: element.id,
+        name: element.name,
+        information: element.information,
+        department: element.department,
+        status: element.status,
+        ikisaki: element.ikisaki,
+        time: element.time,
+        memo: element.memo,
+        email: element.email,
+        updateTime : dstr
+      });
 
-            usertabledata.push({
-                id: element.attributes.id,
-                name: element.attributes.name,
-                information: element.attributes.information,
-                department: element.attributes.department,
-                status: element.attributes.status,
-                ikisaki: element.attributes.ikisaki,
-                time: element.attributes.time,
-                memo: element.attributes.memo,
-                email: element.attributes.email,
-                updateTime : dstr
-            });  
-        });
+  });
 
-        var data = {
-            title: '行先情報一覧',
-            finding : '名前または部署名などを入力',
-            login: req.session.login,
-            usertabledata: usertabledata,
-            datastatus: datastatus,
-            datakyakusaki: datakyakusaki,
-            datashanai: datashanai,
-            msg: datacontact
-        };
-        res.render('index', data);
-    }).catch((err) => { 
-    res.status(500).json({error: true, data: {message: err.message}});
-    });
+  var data = {
+    title: '行先情報一覧',
+    finding : '名前または部署名などを入力',
+    login: req.session.login,
+    usertabledata: usertabledata,
+    datastatus: datastatus,
+    datakyakusaki: datakyakusaki,
+    datashanai: datashanai,
+    msg: datacontact,
+    datadepartment: datadepartment
+  };
+
+  res.render('index', data);   
+  
+  });
+
+  // new Userdata().where({id: 0})
+  //   .fetch()
+  //   .then((collection) => {         
+
+  //     db.serialize(()=> {
+    
+  //       var sql = 'SELECT * , CASE WHEN department = "' + login.department + '" THEN "AA" ELSE department END as sort1, CASE WHEN name = "' + login.name + '" THEN "00" ELSE name END as sort2 FROM users ORDER BY sort1,sort2 ASC;';
+
+  //       db.all(sql, [], (err, rows)=> {
+  //         console.log('db.all(sql, [], (err, rows)開始');
+  //         console.log("sql: " + sql);
+  //         if (!err) {
+    
+  //           rows.forEach(element => {
+    
+  //               var d1 = moment(new Date(element.updated_at));
+  //                   d1.locale('ja');
+  //                   d1.tz('Asia/Tokyo');
+  //               var dstr = d1.fromNow();
+    
+  //               usertabledata.push({
+  //                 id: element.id,
+  //                 name: element.name,
+  //                 information: element.information,
+  //                 department: element.department,
+  //                 status: element.status,
+  //                 ikisaki: element.ikisaki,
+  //                 time: element.time,
+  //                 memo: element.memo,
+  //                 email: element.email,
+  //                 updateTime : dstr
+  //               });
+    
+  //           });
+    
+  //           var data = {
+  //             title: '行先情報一覧',
+  //             finding : '名前または部署名などを入力',
+  //             login: req.session.login,
+  //             usertabledata: usertabledata,
+  //             datastatus: datastatus,
+  //             datakyakusaki: datakyakusaki,
+  //             datashanai: datashanai,
+  //             msg: datacontact,
+  //             datadepartment: datadepartment
+  //           };
+    
+  //           res.render('index', data);
+    
+  //         }
+          
+  //       })
+  //     });
+
+  //   });
 
 });
+
+//SQL router.getはここまで
+
+
+//Bookshelf router.getはここから
+// router.get('/', function(req, res, next) {
+
+//   if (req.session.login == null) {
+//     res.redirect('/login');
+//     return;
+//   }
+
+//   getStatus();
+//   getKyakusaki();
+//   getShanai();
+//   getMsg();
+//   getDepartment();
+
+//   var usertabledata = new Array();
+
+//   new Userdata().orderBy('department','ASC')
+//     .fetchAll()
+//     .then((collection) => {
+//         var content = collection.toArray();
+//         console.log('datacontact' + datacontact);
+//         console.log('datadepartment' + datadepartment);
+        
+//         content.forEach(element => {
+
+//             var d1 = moment(new Date(element.attributes.updated_at));
+//                 d1.locale('ja');
+//                 d1.tz('Asia/Tokyo');
+//             var dstr = d1.fromNow();
+
+//             usertabledata.push({
+//                 id: element.attributes.id,
+//                 name: element.attributes.name,
+//                 information: element.attributes.information,
+//                 department: element.attributes.department,
+//                 status: element.attributes.status,
+//                 ikisaki: element.attributes.ikisaki,
+//                 time: element.attributes.time,
+//                 memo: element.attributes.memo,
+//                 email: element.attributes.email,
+//                 updateTime : dstr
+//             });
+//         });
+
+//         var data = {
+//             title: '行先情報一覧',
+//             finding : '名前または部署名などを入力',
+//             login: req.session.login,
+//             usertabledata: usertabledata,
+//             datastatus: datastatus,
+//             datakyakusaki: datakyakusaki,
+//             datashanai: datashanai,
+//             msg: datacontact,
+//             datadepartment: datadepartment
+//         };
+//         res.render('index', data);
+//     }).catch((err) => { 
+//     res.status(500).json({error: true, data: {message: err.message}});
+//     });
+
+// });
+//Bookshelf router.getはここまで
 
 router.post('/', (req,res,next) => {
 
@@ -158,9 +300,10 @@ router.post('/', (req,res,next) => {
   getKyakusaki();
   getShanai();
   getMsg();
+  getDepartment();
 
   var usertabledata = new Array();
-
+  var find_content = req.body.find;
   new Userdata().orderBy('created_at','DESC')
     .query(function(find){
       find.where('department','like','%' + req.body.find +'%')
@@ -198,14 +341,15 @@ router.post('/', (req,res,next) => {
         });
 
         var data = {
-          title: '検索結果',
+          title: '“' + find_content + '”の検索結果',
           finding : '状態、行先、メモ内容などでも検索可能',
           usertabledata: usertabledata,
           datastatus: datastatus,
           datakyakusaki: datakyakusaki,
           datashanai: datashanai,
           login: req.session.login,
-          msg:datacontact
+          msg:datacontact,
+          datadepartment: datadepartment
       };
       res.render('index', data);
   }).catch((err) => { 
@@ -263,7 +407,6 @@ router.post('/newuser', (req,res,next) => {
 });
 
 // test SQL
-var db = new sqlite3.Database('ikisaki.sqlite3')
 //客先変更
 router.post('/newkyakusaki', (req,res,next) => {
 
@@ -317,6 +460,66 @@ router.post('/newkyakusaki', (req,res,next) => {
       var newkyakusaki = req.body.newkyakusaki;
       db.run('INSERT INTO kyakusaki_list (kyakusaki) values (?)',newkyakusaki);
       console.log("単項目：「" + newkyakusaki + "」の追加完了したのでリダイレクト。");
+      res.redirect('/');
+      return false;
+    }
+  })
+  
+})
+
+//部署変更
+router.post('/newdepartment', (req,res,next) => {
+
+  if (req.session.login == null) {
+    res.redirect('/login');
+    return;
+  }
+
+  console.log(req.body);
+  db.serialize(()=> {
+    var cnt = req.body.cnt;
+    console.log("更新回数は：「 " + cnt + "+1 」回");
+
+    for (var i=0; i<=cnt; i++ ) {
+      var department = req.body.department[i];
+      var id = req.body.id[i];
+      console.log("変更先ID: 「" + id + "」、変更後：「" + department + "」");
+
+      if (department.length == 0) {
+        db.run('DELETE FROM department_list WHERE id = ' + id);
+        console.log("インデックス「" + i + "」の項目削除実行しました。");
+      } else {
+        var q = 'UPDATE department_list SET department = ?  WHERE id = ?';
+        db.run(q, department, id);
+        console.log("インデックス:「" + i + "」の項目を更新しました。");
+      }
+    }
+    // 客先新規
+    if (req.body.newdepartment == undefined || req.body.newdepartment.length == 0) {
+      res.redirect('/');
+      console.log("追加項目無し、リダイレクト。");
+      return false;
+    }
+
+    console.log("新項目数は：「" + req.body.newdepartment.length + "」");
+    if (Array.isArray(req.body.newdepartment)) {
+      for (var n=0; n<req.body.newdepartment.length; n++) {
+        var newdepartment = req.body.newdepartment[n];
+        console.log("req.body.newdepartment[" + n + "]：" + req.body.newdepartment[n]);
+        if (newdepartment.length == 0) {
+          console.log("該当項目は空であるため、追加しません。");
+        } else {
+          db.run('INSERT INTO department_list (department) values (?)',newdepartment);
+          console.log("追加した項目：" + newdepartment);
+        }
+      }
+      console.log("配列追加完了したのでリダイレクト。");
+      res.redirect('/');
+      return false;
+    } else {
+      var newdepartment = req.body.newdepartment;
+      db.run('INSERT INTO department_list (department) values (?)',newdepartment);
+      console.log("単項目：「" + newdepartment + "」の追加完了したのでリダイレクト。");
       res.redirect('/');
       return false;
     }
